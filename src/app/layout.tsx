e9d5d93be6a342ec5aa310ef910a4e8424d02eaf
Nipicoco <villarrica43@GMAIL.COM>
2024-09-17 19:06:55 -0300

    changed title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ import { ThemeProvider } from "@/components/theme-provider"
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "NES Productions",
+  title: "NES Producciones",
   description: "HACIENDO MILLONES",
 }
 
